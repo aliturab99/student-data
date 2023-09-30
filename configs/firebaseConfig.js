e69794b5dfe271ef56a2a4ca0145561f5267a3ac
Syed Yawar Ali Turab <77963884+aliturab99@users.noreply.github.com>
2023-09-30 14:23:46 +0500

    Initializing the Server

---
+++ configs/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAagJ1cNEhtokqfkCcwzC966kwKR6R45Q0",
+  authDomain: "students-data-7cfb3.firebaseapp.com",
+  projectId: "students-data-7cfb3",
+  storageBucket: "students-data-7cfb3.appspot.com",
+  messagingSenderId: "778440708824",
+  appId: "1:778440708824:web:0ba54d273ad1c54af7bdad",
+  measurementId: "G-MW0XSPG5K0"
+};
+
+const app = initializeApp(firebaseConfig);
